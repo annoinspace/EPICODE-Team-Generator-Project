9a842acda0f7b5bd386cb573e8d59bdcd6585a4b
annoinspace <aneesahalmask@gmail.com>
2022-10-28 10:18:09 +0100

    function to add names into box and reset to clear all names

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+const addButton = document.getElementById("add-name-btn")
+const newNameInput = document.getElementById("name-input-box")
+const allNamesContainer = document.getElementById("member-name-box")
+const resetButton = document.getElementById("reset-btn")
 
+function addNewName() {
+  let newName = newNameInput.value
+  let addName = document.createElement("p")
+  addName.innerHTML = newName
+  allNamesContainer.appendChild(addName)
+  newNameInput.value = ""
+}
+
+function resetNames() {
+  while (allNamesContainer.firstChild) {
+    allNamesContainer.removeChild(allNamesContainer.firstChild)
+  }
+}
