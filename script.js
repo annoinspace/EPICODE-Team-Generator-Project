d767db474064d0b2aac10ca46561f5fb7c06ca94
annoinspace <aneesahalmask@gmail.com>
2022-10-28 10:27:46 +0100

    new names get added to array of names with click of add-name-btn

--- script.js
+++ script.js
@@ -3,8 +3,16 @@ const newNameInput = document.getElementById("name-input-box")
 const allNamesContainer = document.getElementById("member-name-box")
 const resetButton = document.getElementById("reset-btn")
 
+let allNames = []
+
 function addNewName() {
   let newName = newNameInput.value
+
+  function addNameToArray() {
+    allNames.push(newName)
+  }
+  addNameToArray()
+
   let addName = document.createElement("p")
   addName.innerHTML = newName
   allNamesContainer.appendChild(addName)
